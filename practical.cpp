fb11d782d9e1a0b860b8d87e98c21ba3ff571164
Jishan Shaikh <30091032+Jishanshaikh4@users.noreply.github.com>
2018-09-17 22:42:46 +0530

    Create practical.cpp

---
+++ practical.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(0);
+
+// Predefined functions may be modular exponentiation, etc.
+
+__int128 main(){
+  FastIO()
+  
+  // Your code goes here.
+  
+  return 0;
+}
